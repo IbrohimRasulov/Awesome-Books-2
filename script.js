06b5ba1eb9870b3d6508710b2459c5e8614c3395
LOctavio <luis_ram1997@hotmail.com>
2021-07-12 14:57:12 -0500

    Create addBook function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const bookList = []
+
+function book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addBook(title, author) {
+  const book = new Book(title, author);
+  bookList.push(book);
+}
+
