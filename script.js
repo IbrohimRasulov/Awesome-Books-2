3e2057c0964e33bc2af4b95678e77898582a1d45
LOctavio <luis_ram1997@hotmail.com>
2021-07-13 14:12:27 -0500

    Refactor for using classes

--- script.js
+++ script.js
@@ -1,56 +1,64 @@
-let bookList = [];
+class Books {
+  constructor() {
+    this.bookList = [];
+  }
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
+  addBook(title, author) {
+    const book = {title, author};
+    this.bookList.push(book);
+  }
 
-function addBook(title, author) {
-  const book = new Book(title, author);
-  bookList.push(book);
-}
+  addBookToStorage(title, author) {
+    window.localStorage.setItem(title, author);
+  }
 
-function filterByTitle(item) {
-  return item.title !== 'title 1';
-}
+  display(title, author) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>${title}</p>
+    <p>${author}</p>
+    <button class = "delete-buttons">Delete</button>`;
+    const bookContainer = document.getElementById('bookContainer');
+    bookContainer.appendChild(div);
+  }
 
-function addBookToStorage(title, author) {
-  window.localStorage.setItem(title, author);
-}
+  removeBook(title, e) {
+    this.bookList = this.bookList.filter(this.filterByTitle);
+    e.parentNode.parentNode.removeChild(e.parentNode);
+    this.revomeFromStorage(title);
+  }
 
-function revomeFromStorage(title) {
-  window.localStorage.removeItem(title);
-}
+  filterByTitle(item) {
+    return item.title !== 'title 1';
+  }
 
-function removeBook(title, e) {
-  bookList = bookList.filter(filterByTitle);
-  e.parentNode.parentNode.removeChild(e.parentNode);
-  revomeFromStorage(title);
-}
-
-function display(title, author) {
-  const div = document.createElement('div');
-  div.innerHTML = `
-  <p>${title}</p>
-  <p>${author}</p>
-  <button onClick = "removeBook('${title}', this)">Delete</button>`;
-  const bookContainer = document.getElementById('bookContainer');
-  bookContainer.appendChild(div);
+  revomeFromStorage(title) {
+    window.localStorage.removeItem(title);
+  }
 }
 
 for (let i = 0; i < localStorage.length; i += 1) {
   const title = localStorage.key(i);
   const author = localStorage.getItem(localStorage.key(i));
-  display(title, author);
+  const book = new Books();
+  book.display(title, author);
 }
 
 document.getElementById('book-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.getElementById('book-title').value;
   const author = document.getElementById('author').value;
-  addBook(title, author);
-  display(title, author);
-  addBookToStorage(title, author);
+  const book = new Books();
+  book.addBook(title, author);
+  book.display(title, author);
+  book.addBookToStorage(title, author);
   document.getElementById('book-form').reset();
 });
-removeBook();
+
+
+const dbtn = document.querySelector('#bookContainer');
+dbtn.addEventListener('click',(e) => {
+  const book = new Books();
+  const title = e.target.parentNode.firstChild.nextSibling
+  book.removeBook(title.textContent, title)
+} );
