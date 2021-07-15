0043a0de53a88bbd58139696ebd51a0fab5e0f07
LOctavio <luis_ram1997@hotmail.com>
2021-07-15 15:16:37 -0500

    Add function for printing current time with luxon

---
+++ dateTime.js
@@ -0,0 +1,7 @@
+import { DateTime } from "./luxon.js";
+
+function printTime() {
+    const currentDate = document.getElementById('currentDate');
+    currentDate.innerHTML = `<div>${DateTime.now().toFormat('MMMM dd yyyy, hh:mm:ss')}</div>`;
+}
+setInterval(printTime, 1000);
