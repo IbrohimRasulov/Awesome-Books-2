40a46e5ef70a3aedba8804df290defaaef80b9f5
LOctavio <luis_ram1997@hotmail.com>
2021-07-15 16:29:13 -0500

    Add script for luxon

--- dateTime.js
+++ dateTime.js
@@ -1,7 +1,5 @@
-import { DateTime } from 'https://moment.github.io/luxon/es6/luxon.js';
-
 function printTime() {
   const currentDate = document.getElementById('currentDate');
-  currentDate.innerHTML = `<div>${DateTime.now().toFormat('MMMM dd yyyy, hh:mm:ss')}</div>`;
+  currentDate.innerHTML = `<div>${luxon.DateTime.now().toFormat('MMMM dd yyyy, hh:mm:ss')}</div>`;
 }
 setInterval(printTime, 1000);
