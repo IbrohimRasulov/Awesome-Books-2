8352ed4fad7215cccc1b12feb29bb7a644e20e17
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-07-16 02:46:03 +0500

    Fix linter errors

--- dateTime.js
+++ dateTime.js
@@ -1,5 +1,6 @@
 function printTime() {
   const currentDate = document.getElementById('currentDate');
+  // eslint-disable-next-line no-undef
   currentDate.innerHTML = `<div>${luxon.DateTime.now().toFormat('MMMM dd yyyy, hh:mm:ss')}</div>`;
 }
 setInterval(printTime, 1000);
